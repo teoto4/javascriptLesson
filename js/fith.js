19e9afa0aa8f2e9029d087219d7182e8d71fe0c1
Revy <162366071+teoto4@users.noreply.github.com>
2024-06-18 17:30:33 +0300

    concat

--- js/fith.js
+++ js/fith.js
@@ -1,15 +1,10 @@
 const arrayFirst = [5, 10, 15, 20];
 const arraySecond = [10, 20, 30];
-let resultFirst = 0;
-let resultSecond = 0;
-let finalResult = 0;
+const concatArray = arrayFirst.concat(arraySecond);
+let result = 0;
 
-for (const numberFirst of arrayFirst) {
-  resultFirst += numberFirst;
-}
-for (const numberSecond of arraySecond) {
-  resultSecond += numberSecond;
+for (number of concatArray) {
+  result += number;
 }
 
-finalResult = resultFirst + resultSecond;
-console.log(finalResult);
+console.log(result);
